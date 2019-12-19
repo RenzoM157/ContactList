771208d08f2550be410780309b631c1911436819
RenzoM157 <45223005+RenzoM157@users.noreply.github.com>
2019-12-19 17:12:12 -0600

    Add files via upload

---
+++ Contacts/Contacts/dbo.People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NULL, 
+    [Company] NVARCHAR(100) NULL, 
+    [Telephone] NVARCHAR(100) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL
+)
